8e367511e20ee18719a1835e3144331a9f454db8
Milan Kragujević <milankragujevic@gmail.com>
2019-07-20 18:22:43 +0200

    Update omni_a505fn.mk

--- omni_a505fn.mk
+++ omni_a505fn.mk
@@ -9,14 +9,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     device/samsung/a505fn/recovery.fstab:recovery/root/recovery.fstab
 
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
-PRODUCT_COPY_FILES += \
-    device/samsung/a505fn/init.recovery.exynos9610.rc:recovery/root/init.recovery.exynos9610.rc
-
-PRODUCT_COPY_FILES += \
-    device/samsung/a505fn/init.recovery.usb.rc:recovery/root/init.recovery.usb.rc
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 PRODUCT_DEVICE := a505fn
 PRODUCT_NAME := omni_a505fn
 PRODUCT_BRAND := samsung
